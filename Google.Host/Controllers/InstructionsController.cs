2dfc6eca9797a6788c5902424c1eddb146883005
rfreiday <business@freiday.com>
2016-06-10 10:02:02 -0400

    Add files via upload

---
+++ Google.Host/Controllers/InstructionsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Google.Host.Controllers
+{
+    public class InstructionsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Google.Host/Controllers/SearchController.cs
+++ Google.Host/Controllers/SearchController.cs
@@ -21,7 +21,7 @@ namespace Google.Host.Controllers
             {
                 try
                 {
-                    WebKeyword keyword = Indexer.Keywords.Where(o => o.Key.Equals(searchValue, StringComparison.OrdinalIgnoreCase)).FirstOrDefault().Value;
+                    WebKeyword keyword = Indexer.SearchKeyword(searchValue);
                     if (keyword != null)
                         return View(keyword);
                     else
